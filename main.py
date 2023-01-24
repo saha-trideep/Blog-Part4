f9910ab44ff147d4944a08bd4cd7d358489d2c66
Trideep <trideepsaha009@gmail.com>
2023-01-24 20:32:15 +0530

    app transfer commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 Bootstrap4(app)
 ckeditor = CKEditor(app)

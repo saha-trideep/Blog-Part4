2cf6b05e19726a40a6f6cfc5914e965103168e57
Trideep <trideepsaha009@gmail.com>
2023-01-26 00:13:33 +0530

    upgrade database commit

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ class Comment(db.Model):
     parent_post = relationship('BlogPost', back_populates='all_comments')
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 def admin_only(f):

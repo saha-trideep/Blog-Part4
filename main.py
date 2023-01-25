5672de9fced1a29119d28c938351826ba30a3925
Trideep <trideepsaha009@gmail.com>
2023-01-25 19:48:20 +0530

    primary changes commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import gunicorn
 from sqlalchemy.exc import IntegrityError
+from click.core import Parameter
 
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
